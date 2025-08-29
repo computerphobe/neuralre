fd865f35f03b64bd7e36def07d53a373d19735a4
computerphobe <aryanbharadiyak2006@gmail.com>
2025-08-29 19:22:08 +0530

    Trying to use docker

--- app/gui.py
+++ app/gui.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import os
-from static_analysis.r2pipeline import analyse_binary
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from ..static_analysis.r2pipeline import analyse_binary
 from core.engine import prompt_builder
 from llm.gemini import gemini_query
 
